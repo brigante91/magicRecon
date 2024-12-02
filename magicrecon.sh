1c7931444132a3c5f1df468d3c876ff68c34a4ea
brigante <g.leo@swascan.com>
2024-12-02 12:05:48 +0100

    eliminato:              CODE_OF_CONDUCT.md 	eliminato:              CONTRIBUTING.md 	eliminato:              README.md 	modificato:             configuration.cfg 	eliminato:              images/poc.gif 	modificato:             magicrecon.sh 	magicrecon_final.sh 	magicrecon_parallel.sh 	magicrecon_parallel_complete.sh 	targets/

--- magicrecon.sh
+++ magicrecon.sh
@@ -55,7 +55,8 @@ passive_recon(){
 	
 	printf "\n${GREEN}[+] Horizontal domain correlation/acquisitions ${NORMAL}\n"
 	printf "${NORMAL}${CYAN}Searching horizontal domains...${NORMAL}\n\n"
-	email=$(whois $domain | grep "Registrant Email" | egrep -ho "[[:graph:]]+@[[:graph:]]+")
+	#email=$(whois $domain | grep "Registrant Email" | egrep -ho "[[:graph:]]+@[[:graph:]]+")
+	email=$(whois $domain | grep "Registrant Email" | grep -E "[[:graph:]]+@[[:graph:]]+")
 	curl -s -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36" "https://viewdns.info/reversewhois/?q=$email" | html2text | grep -Po "[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)" | tail -n +4  | head -n -1 
 	
 	printf "\n${GREEN}[+] ASN Lookup ${NORMAL}\n"
